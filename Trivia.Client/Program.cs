ab45ba6c73c9041e47093ed8ef20010a216f7a0c
Bart Bovendeerdt <bartbovendeerdt@gmail.com>
2018-10-31 20:33:46 +0100

    initial commit

---
+++ Trivia.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Trivia.Client
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            // Set correct settings for Application Insights
+            TelemetryConfiguration.Active.InstrumentationKey =
+                ConfigurationManager.AppSettings["ApplicationInsightsInstrumentationKey"];
+
+
+            Console.WriteLine(
+                "████████╗██████╗ ██╗██╗   ██╗██╗ █████╗ \r\n╚══██╔══╝██╔══██╗██║██║   ██║██║██╔══██╗\r\n   ██║   ██████╔╝██║██║   ██║██║███████║\r\n   ██║   ██╔══██╗██║╚██╗ ██╔╝██║██╔══██║\r\n   ██║   ██║  ██║██║ ╚████╔╝ ██║██║  ██║\r\n   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝  ╚═╝╚═╝  ╚═╝\r\n                                        " +
+                "\r\n\r\n");
+
+            Console.ReadLine();
+
+            // Write your logic here :)
+        }
+    }
+}
